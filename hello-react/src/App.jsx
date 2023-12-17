3901fb3fcebce50e44a89e9becd838dd16cf4ff2
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2023-12-18 00:44:23 +0530

    L1

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -3,25 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import TaskCard from './TaskCard'
-
+import './index.css'
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <div>
-        <h1>Smarter Tasks</h1>
-        <p><b>Project:</b> Graduation Final Year Project (Revamp College Website) </p>
-        <h1>Pending</h1>
-        <TaskCard title="Task 3" dueDate="10th April" assigneeName="Rohit S" isDone={false}/>
-        <TaskCard title="Task 4" dueDate="22nd March" assigneeName="Rohit M" isDone={false}/>
+        <h1 className="font-bold text-4xl">Smarter Tasks</h1>
+        <p className='text-gray-600'><b>Project:</b> Graduation Final Year Project (Revamp College Website) </p>
       </div>
-      <div>
-        <h1>Done</h1>
-        <TaskCard title="Task 1" completedAtDate="10th April" assigneeName="Rohit M" isDone={true}/>
-        <TaskCard title="Task 2" completedAtDate="20th April" assigneeName="Ajay S" isDone={true}/>
+      <div className="flex justify-center mt-8">
+        <div className="border p-4 rounded-xl">
+          <h1 className="font-bold text-3xl mb-3 text-gray-700">Pending</h1>
+          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S" isDone={false}/>
+          <TaskCard title="Add a blog" dueDate="22nd March" assigneeName="Rohit M" isDone={false}/>
+        </div>
+        <div className="border p-4 rounded-xl ml-4">
+          <h1 className="font-bold text-3xl mb-3 text-gray-700">Done</h1>
+          <TaskCard title="Design the mockup" completedAtDate="10th April" assigneeName="Rohit M" isDone={true}/>
+          <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S" isDone={true}/>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 
