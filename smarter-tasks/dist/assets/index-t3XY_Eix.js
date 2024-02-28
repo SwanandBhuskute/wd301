cc7d11754d734bbb9111ef85a61e58b71b957041
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2024-02-28 18:02:52 +0530

    L9

--- smarter-tasks/dist/assets/index-t3XY_Eix.js
+++
@@ -1,7 +0,0 @@
-import{R as r,_ as e,j as s,r as a,O as t}from"./index-HuJDM1LC.js";import{T as o}from"./context-A1qgozf3.js";import{E as i}from"./ErrorBoundary-Y7f56Z7d.js";const l=r.lazy(()=>e(()=>import("./ProjectDetails-asjVotab.js"),__vite__mapDeps([0,1,2,3,4]))),x=()=>s.jsxs(o,{children:[s.jsx(i,{children:s.jsx(a.Suspense,{fallback:s.jsx("div",{className:"suspense-loading",children:"Loading..."}),children:s.jsx(l,{})})}),s.jsx(t,{})]});export{x as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/ProjectDetails-asjVotab.js","assets/index-HuJDM1LC.js","assets/index-o2TAB8We.css","assets/context-A1qgozf3.js","assets/actions-1oMZxqAw.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- smarter-tasks/src/pages/members/MemberList.tsx
+++ smarter-tasks/src/pages/members/MemberList.tsx
@@ -1,17 +1,24 @@
+import React, { Suspense } from "react";
 import { fetchMembers } from "../../context/members/actions";
 import { useEffect } from "react";
 import { useMembersDispatch } from "../../context/members/context";
 import MemberListItems from './MemberListItems';
+import ErrorBoundary from "../../components/ErrorBoundary";
 
 const MemberList: React.FC = () => {
   const dispatchMembers = useMembersDispatch();
+
   useEffect(() => {
-    fetchMembers(dispatchMembers)
-  }, [])
-  
+    fetchMembers(dispatchMembers);
+  }, []);
+
   return (
     <div className="grid grid-cols-4 gap-4 mt-5">
-      <MemberListItems />
+      <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <MemberListItems />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
